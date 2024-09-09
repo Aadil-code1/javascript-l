b06f25465e8f2ef2290768b8f727e4890bb533df
Aadil-code1 <ahmedaadil438@gmail.com>
2024-09-09 14:08:03 +0000

    datatypes begining

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+
+//  "use strict";  //treats the code in newer version of javascript
+
+// //  alert(3 +3) we are using node js not browser
+
+// console.log(
+//     3
+//     +3 // code readabilty
+// )
+
+// number 2*53
+// BigInt
+// string ""
+// boolean  true/false
+// null => standalone value
+// symbol =>unique
+// object
+
+console.log( null);
+
+
