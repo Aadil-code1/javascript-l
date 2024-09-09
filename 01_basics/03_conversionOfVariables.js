929ff59fe5427268bca7c7c1d470190de72181c5
Aadil-code1 <ahmedaadil438@gmail.com>
2024-09-09 16:24:03 +0000

    summary of datatypes for interview

---
+++ 01_basics/03_conversionOfVariables.js
@@ -0,0 +1,13 @@
+// let score = "44abc"
+// let convertVariable = Number(score);
+// console.log(typeof(score));
+
+// "33"=>33
+//  "33abc" =>NaN
+// true =>1; false =>0
+
+
+isLoggedIn = 1
+userName = "Aadil"
+let booleanIsLoggedIn = Boolean(userName)
+console.log(booleanIsLoggedIn);
---
+++ 01_basics/04_dataTypesSummary.js
@@ -0,0 +1,43 @@
+// Interview perspective
+
+// primitive datatype
+// 7 types : String, Number, Boolean,  null,  undefined,
+//           symbol,BigInt   [SNB    NUSB]
+
+// const Id = Symbol("123")
+// const userId =Symbol("123")
+// console.log(Id===userId)
+
+// const BigInt = 9828875377489n
+
+// Reference (Non Primitive) 
+// Array, Object, Function [AOF]
+
+// console.log(typeof(Array)) function
+// console.log(typeof(Object))  function
+// console.log(typeof(Function))  function
+
+
+
+
+const friends=  ["Aadil","Anas","kunal",5]
+console.log(friends)
+let myObj = {
+    name:"Aadil",
+    age:18,
+    school: "coding",
+    Laptop:true
+}
+// console.log(myObj)
+const myFunction = function(){
+    // console.log("Hello World!")
+}
+//  console.log(typeof(bigInt))  undefined 
+//  console.log(typeof(null))  object
+
+
+
+
+// why javascript is dynamically type
+// it automatically assigns thevalue of data type at run time
+
