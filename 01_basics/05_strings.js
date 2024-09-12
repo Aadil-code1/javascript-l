3184053dc3f24e0f21413600c08745c38a755d1d
Aadil-code1 <ahmedaadil438@gmail.com>
2024-09-12 16:17:44 +0000

    nums

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name = "Aadil";
+const  repoCount = 50;
+
+// console.log( name + "  " +  repoCount);
+
+// console.log(`my name is ${name} and 
+//     and my repo count is ${repoCount}`);
+const newGame = String('mario');
+// console.log(newGame[0]);
+// console.log(newGame.__proto__)
+// console.log(newGame.toUpperCase())
+// console.log(newGame.length)
+// console.log(newGame.charAt(2))
+// console.log(newGame.indexOf('r'))
+// const newString = newGame.substring(0,4);
+// console.log(newString)
+// const anotherString =  newGame.slice(-4,3)
+// console.log(anotherString)
+
+const brother =  String('    anas   ')
+// console.log (brother)
+console.log(brother.trim())
+const url = "https://aadil%20ahmed.com"
+console.log(url.replace('aadil%20','me'))
---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,17 @@
+const num = 400
+
+console.log (num);
+
+const balance = new Number(500)
+console.log(balance);
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(3));
+
+const anotherNumber = 32.98
+
+console.log(anotherNumber.toPrecision(4))
+
+const hundreds = 100000;
+
+console.log(hundreds.toLocaleString('en-IN'));
