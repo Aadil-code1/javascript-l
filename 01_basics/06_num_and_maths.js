2aabb2c81d2acb2331ff47248c58f3b7a6cf6954
Aadil-code1 <ahmedaadil438@gmail.com>
2024-09-16 05:23:57 +0000

    math function

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -1,17 +1,23 @@
-const num = 400
+// const num = 400
 
-console.log (num);
+// console.log (num);
 
-const balance = new Number(500)
-console.log(balance);
-console.log(balance.toString().length);
+// const balance = new Number(500)
+// console.log(balance);
+// console.log(balance.toString().length);
 
-console.log(balance.toFixed(3));
+// console.log(balance.toFixed(3));
 
-const anotherNumber = 32.98
+// const anotherNumber = 32.98
 
-console.log(anotherNumber.toPrecision(4))
+// console.log(anotherNumber.toPrecision(4))
 
-const hundreds = 100000;
+// const hundreds = 100000;
 
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(Math.random());
+console.log((Math.random()*10)+1);
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random()*(max - min +1)) + min);
